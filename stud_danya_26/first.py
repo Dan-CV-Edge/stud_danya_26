47406db7aed6752ab01c4cd94ff31d164726a2d0
Danil <vf.vizard@mail.ru>
2026-04-13 22:14:30 +0300

    Выполнена Лабораторная работа №1: узел first_node

---
+++ stud_danya_26/first.py
@@ -0,0 +1,13 @@
+import rclpy
+from rclpy.node import Node
+
+def main(args=None):
+    rclpy.init(args=args)                 
+    node = Node('hello_node')           
+    node.get_logger().info("Hello ROS 2 World! 🚀")
+    rclpy.spin(node)                       
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
